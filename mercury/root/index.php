6ce131725bde858a4dab09aba02b44b5d2108e7f
90N45 <79598596+90N45-d3v@users.noreply.github.com>
2023-01-16 21:22:08 +0100

    Title + Animation performance boost

--- mercury/root/index.php
+++ mercury/root/index.php
@@ -72,11 +72,11 @@ if($_COOKIE[$cookie_name] != $token) {
 		<iframe src="chat.php" class="rounded" height="65%" width="100%" title="ChatFrame" id="chat" style="animation-name: slide-right; animation-duration: 1.5s; position: relative;"></iframe>
 		<iframe name="dummyframe" id="dummyframe" style="display: none;"></iframe>
 		<br><br>
-		<form method="post" action="send_message.php" target="dummyframe">
+		<form method="post" action="send_message.php" target="dummyframe" style="animation-name: slide-left; animation-duration: 1.5s; position: relative;">
 			<code class="d-flex justify-content-between h-25">
-				<input class="rounded" type="text" name="nickname" id="nickname" placeholder=" Name" style="color: #FFBF80; background-color: #1a1a1a; border: none; outline: none; height: 20%; width: 20%; animation-name: slide-left; animation-duration: 1.5s; position: relative; font-size: 12px;">
-				<input class="rounded" type="text" name="message" id="message" placeholder=" Type something..." style="color: #FFBF80; background-color: #1a1a1a; border: none; outline: none; height: 20%; width: 60%; animation-name: slide-left; animation-duration: 1.5s; position: relative; font-size: 12px;">
-				<button class="rounded" type="submit" id="submit" style="color: #FFBF80; background-color: #1a1a1a; border: none; outline: none; height: 20%; width: 15%; animation-name: slide-left; animation-duration: 1.5s; position: relative;"><img src="send_icon.png" height="40%"/></button>
+				<input class="rounded" type="text" name="nickname" id="nickname" placeholder=" Name" style="color: #FFBF80; background-color: #1a1a1a; border: none; outline: none; height: 20%; width: 20%; font-size: 12px;">
+				<input class="rounded" type="text" name="message" id="message" placeholder=" Type something..." style="color: #FFBF80; background-color: #1a1a1a; border: none; outline: none; height: 20%; width: 60%; font-size: 12px;">
+				<button class="rounded" type="submit" id="submit" style="color: #FFBF80; background-color: #1a1a1a; border: none; outline: none; height: 20%; width: 15%;"><img src="send_icon.png" height="40%"/></button>
 			</code>
 		</form>
 	</div>
