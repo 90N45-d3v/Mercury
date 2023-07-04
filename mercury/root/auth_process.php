7512a42b29465dd2500d1410fda4b0974bf22e15
90N45 <79598596+90N45-d3v@users.noreply.github.com>
2023-07-04 21:39:23 +0200

    Admin Panel

--- mercury/root/auth_process.php
+++ mercury/root/auth_process.php
@@ -16,9 +16,39 @@ if (str_contains($token, $date)) {
 }
 
 if ($passwd == "doritos") {
+    $path = "../admin/Logs/success_login.csv";
+
+    if (count(file($path)) == 150) {
+        $log = file_get_contents($path);
+        $new = substr($log, strpos($log, "\n")+1);
+        file_put_contents($path, $new);
+    }
+
+    $date = date("Y.m.d");
+    $time = date("H:i:s");
+
+    $log = file_get_contents($path);
+    $log .= $date . "," . $time . "," . $_SERVER['REMOTE_ADDR'] . ",\"" . $_SERVER['REMOTE_HOST'] . "\",\"" . $_SERVER['HTTP_USER_AGENT'] . "\"\n";
+    file_put_contents($path, $log);
+
     setcookie("mercury_auth", $token);
     header("Location: /index.php");
 } else {
+    $path = "../admin/Logs/fail_login.csv";
+
+    if (count(file($path)) == 100) {
+        $log = file_get_contents($path);
+        $new = substr($log, strpos($log, "\n")+1);
+        file_put_contents($path, $new);
+    }
+
+    $date = date("Y.m.d");
+    $time = date("H:i:s");
+
+    $log = file_get_contents($path);
+    $log .= $date . "," . $time . "," . $_SERVER['REMOTE_ADDR'] . ",\"" . $_SERVER['REMOTE_HOST'] . "\",\"" . $_SERVER['HTTP_USER_AGENT'] . "\"\n";
+    file_put_contents($path, $log);
+
     header("Location: /authentication.html");
 }
 ?>
